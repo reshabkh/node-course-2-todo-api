341415f3d2f5df7e66ad7fb0f31bd5fd9b24b8b1
Reshab Khandelwal <reshab>
2017-10-24 13:36:27 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var bodyParser =require('body-parser');
-var {ObjectId} = require('mongodb');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser =require('body-parser');
+const {ObjectId} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose.js');
@@ -61,6 +63,32 @@ Todo.findByIdAndRemove(id).then((todo) => {
   res.status(400).send(e);
 });
 });
+
+app.patch('/todos/:id',(req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+
+  if(!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set : body},{new:true}).then((todo) =>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
 app.listen(port,() => {
   console.log(`you statred at ${port} id`);
 });
