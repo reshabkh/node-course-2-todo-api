f0745903cad09852d3f48978d577f75132725894
Reshab Khandelwal <reshab>
2017-10-24 12:34:52 +0530

    Test the DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -96,3 +96,42 @@ describe('GET /todos/:id',()=>{
       .end(done)
   });
 });
+
+describe('DELETE /todos/:id',() => {
+  it('should reamove a todo',(done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res)=> {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err,res) => {
+        if(err){
+          return done(err);
+        }
+        Todo.findById(hexId).then((todo) =>{
+          expect(todo).toNotExist();
+          done();
+        }).catch((e)=>done(e));
+      });
+  });
+
+  it('it should return 404 if todo not found',(done) => {
+    var hexId = new ObjectId().toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(404)
+      .end(done)
+  });
+
+  it('it shuld return 404 if id is not valid',(done) => {
+    request(app)
+      .delete('/todos/1224')
+      .expect(404)
+      .end(done)
+  });
+
+});
