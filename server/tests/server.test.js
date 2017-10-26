61f6202b65381561676ab2b3feffbd35f69c2e84
Reshab Khandelwal <reshab>
2017-10-26 19:08:35 +0530

    Add tests for POST  /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -238,3 +238,50 @@ describe('POST /users',() => {
   });
 
 });
+describe('POST /users/login',()=>{
+  it('it should login user and return authenticate token',(done)=>{
+      request(app)
+        .post('/users/login')
+        .send({
+          email:users[1].email,
+          password:users[1].password
+        })
+        .expect(200)
+        .expect((res)=>{
+          expect(res.header['x-auth']).toBeTruthy();
+        })
+        .end((err,res)=>{
+          if(err){
+            return done(err);
+            }
+            User.findById(users[1]._id).then((user)=>{
+              expect(user.tokens[0]).toInclude({
+                access : 'auth',
+                token : res.header['x-auth']
+              });
+              done();
+            }).catch((e)=>done(e));
+        });
+  });
+  it('should reject invalid login',(done)=>{
+    request(app)
+      .post('/users/login')
+      .send({
+        email:users[1].email,
+        password:users[1].password + '1'
+      })
+      .expect(400)
+      .expect((res)=>{
+        expect(res.header['x-auth']).toNotExist();
+      })
+      .end((err,res)=>{
+        if(err){
+          return done(err);
+          }
+          User.findById(users[1]._id).then((user)=>{
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e)=>done(e));
+      });
+  });
+});
